74578a829fe68e52d9ae8d953ba05a7f5a3ab3af
huzaifafaruqui <huzaifafaruqui@outlook.com>
2017-10-10 22:06:16 +0530

    added question 3

---
+++ circleMove.cpp
@@ -0,0 +1,147 @@
+// Source http://www3.ntu.edu.sg/home/ehchua/programming/opengl/cg_introduction.html
+
+#include <windows.h>
+#include <GL/glut.h>
+#include <math.h>     // Needed for sin, cos
+#define PI 3.14159265f
+
+using namespace std;
+
+// Global variables
+char title[] = "Moving Circle";  // Windowed mode's title
+int windowWidth = 640;     // Windowed mode's width
+int windowHeight = 480;     // Windowed mode's height
+int windowPosX = 50;      // Windowed mode's top-left corner x
+int windowPosY = 50;      // Windowed mode's top-left corner y
+
+GLfloat ballRadius = 0.5f;   // Radius of the bouncing ball
+GLfloat ballX = 0.0f;         // Ball's center (x, y) position
+GLfloat ballY = 0.0f;
+GLfloat ballXMax, ballXMin, ballYMax, ballYMin; // Ball's center (x, y) bounds
+GLfloat xSpeed = 0.02f;      // Ball's speed in x and y directions
+GLfloat ySpeed = 0.007f;
+int refreshMillis = 30;      // Refresh period in milliseconds
+
+							 // Projection clipping area
+GLdouble clipAreaXLeft, clipAreaXRight, clipAreaYBottom, clipAreaYTop;
+
+/* Initialize OpenGL Graphics */
+void initGL() {
+	glClearColor(0.0, 0.0, 0.0, 1.0); // Set background (clear) color to black
+}
+
+/* Callback handler for window re-paint event */
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);  // Clear the color buffer
+	// https://stackoverflow.com/questions/4566558/why-is-there-a-glmatrixmode-in-opengl
+	glMatrixMode(GL_MODELVIEW);    // To operate on the model-view matrix
+	glLoadIdentity();              // Reset model-view matrix
+
+	glTranslatef(ballX, ballY, 0.0f);  // Translate to (xPos, yPos)
+									   // Use triangular segments to form a circle
+	glBegin(GL_TRIANGLE_FAN); // https://stackoverflow.com/questions/8043923/gl-triangle-fan-explanation
+	glColor3f(0.0f, 0.0f, 1.0f);  // Blue
+	glVertex2f(0.0f, 0.0f);       // Center of circle
+	int numSegments = 100;
+	GLfloat angle;
+	for (int i = 0; i <= numSegments; i++) { // Last vertex same as first vertex
+		angle = i * 2.0f * PI / numSegments;  // 360 deg for all segments
+		glVertex2f(cos(angle) * ballRadius, sin(angle) * ballRadius);
+	}
+	glEnd();
+
+	// https://stackoverflow.com/questions/28298540/difference-between-single-bufferedglut-single-and-double-buffered-drawingglut
+	// https://stackoverflow.com/questions/6314197/advantages-and-disadvantages-when-using-doublebuffer-on-control
+	glutSwapBuffers();  // Swap front and back buffers (of double buffered mode)
+}
+
+/* Call back when the windows is re-sized */
+void reshape(GLsizei width, GLsizei height) {
+	// Compute aspect ratio of the new window
+	if (height == 0) height = 1;                // To prevent divide by 0
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+
+	// Set the viewport to cover the new window
+	glViewport(0, 0, width, height);
+
+	// Set the aspect ratio of the clipping area to match the viewport
+	glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+	glLoadIdentity();             // Reset the projection matrix
+	if (width >= height) {
+		clipAreaXLeft = -1.0 * aspect;
+		clipAreaXRight = 1.0 * aspect;
+		clipAreaYBottom = -1.0;
+		clipAreaYTop = 1.0;
+	}
+	else {
+		clipAreaXLeft = -1.0;
+		clipAreaXRight = 1.0;
+		clipAreaYBottom = -1.0 / aspect;
+		clipAreaYTop = 1.0 / aspect;
+	}
+	gluOrtho2D(clipAreaXLeft, clipAreaXRight, clipAreaYBottom, clipAreaYTop);
+	ballXMin = clipAreaXLeft + ballRadius;
+	ballXMax = clipAreaXRight - ballRadius;
+	ballYMin = clipAreaYBottom + ballRadius;
+	ballYMax = clipAreaYTop - ballRadius;
+}
+
+
+void keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case 27:     // ESC key
+		exit(0);
+		break;
+	}
+}
+
+/* Callback handler for special-key event */
+void specialKeys(int key, int x, int y) {
+	switch (key) {
+	
+	case GLUT_KEY_RIGHT:    // Right: increase x speed
+		ballX += xSpeed;
+		if (ballX > ballXMax)
+			ballX = ballXMax;
+		break;
+	case GLUT_KEY_LEFT:     // Left: decrease x speed
+		ballX -= xSpeed;
+		if (ballX < ballXMin)
+			ballX = ballXMin; 
+		break;
+	case GLUT_KEY_DOWN:       // Up: increase y speed
+		ballY -= ySpeed;
+		if (ballY < ballYMin)
+			ballY = ballYMin; 
+		break;
+	case GLUT_KEY_UP:     // Down: decrease y speed
+		ballY += ySpeed;
+		if (ballY > ballYMax)
+			ballY = ballYMax;
+		break;
+	
+	}
+}
+
+/* Called back when the timer expired */
+void Timer(int value) {
+	glutPostRedisplay();    // Post a paint request to activate display()
+	glutTimerFunc(refreshMillis, Timer, 0); // subsequent timer call at milliseconds
+}
+
+/* Main function: GLUT runs as a console application starting at main() */
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);            // Initialize GLUT
+	glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
+	glutInitWindowSize(windowWidth, windowHeight);  // Initial window width and height
+	glutInitWindowPosition(windowPosX, windowPosY); // Initial window top-left corner (x, y)
+	glutCreateWindow(title);      // Create window with given title
+	glutDisplayFunc(display);     // Register callback handler for window re-paint
+	glutReshapeFunc(reshape);     // Register callback handler for window re-shape
+	glutTimerFunc(0, Timer, 0);   // First timer call immediately
+	glutSpecialFunc(specialKeys); // Register callback handler for special-key event
+	glutKeyboardFunc(keyboard);   // Register callback handler for special-key event
+	initGL();                     // Our own OpenGL initialization
+	glutMainLoop();               // Enter event-processing loop
+	return 0;
+}
